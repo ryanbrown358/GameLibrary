d7ae1fade3b5152894e91f125da5ec767b1cbe5e
Ryan Brown <coderbrown@Ryans-MacBook-Pro.local>
2019-03-09 11:04:05 -0700

    InitialCommit

---
+++ GameLibrary-SPA/src/app/Components/game/game.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  games: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getGames();
+  }
+
+  getGames(){
+    return this.http.get('http://localhost:5000/api/games').subscribe(res => {
+      this.games = res;
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+}
